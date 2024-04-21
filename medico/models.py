2951dd7f0a1e8b415159e62c2acd40c85d07147b
milenamuch <milenamuchenski@gmail.com>
2024-04-21 20:49:10 -0300

    Aula3_ até o upload de arquivos

--- medico/models.py
+++ medico/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+
 def is_medico(user):
         return DadosMedico.objects.filter(user=user).exists()
 
